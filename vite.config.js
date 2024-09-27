cc59c3d801ee77d229d25c43f42bf358ac50fbb7
teraevradmir100@gmail.com <teraevradmir100@gmail.com>
2024-09-27 15:51:27 +0500

    32

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                album: resolve(__dirname, 'pages/album'),
-                artist: resolve(__dirname, 'pages/artist'),
-                profile: resolve(__dirname, 'pages/profile'),
-                search: resolve(__dirname, 'pages/search'),
-                login: resolve(__dirname, 'pages/login'),
+                album: resolve(__dirname, 'pages/album/index.html'),
+                artist: resolve(__dirname, 'pages/artist/index.html'),
+                profile: resolve(__dirname, 'pages/profile/index.html'),
+                search: resolve(__dirname, 'pages/search/index.html'),
+                login: resolve(__dirname, 'pages/login/index.html'),
             },
         },
     },
